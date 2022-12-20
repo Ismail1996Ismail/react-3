aaf68eb08a767d30ca8485dd6f2eb950160fa5b1
Ismail Buev <ismail.buev@mail.ru>
2022-12-20 22:30:42 +0300

    first commit

---
+++ src/Base.jsx
@@ -0,0 +1,10 @@
+
+function Base(){
+  return(
+    <div>
+      <h1>Reeeeeee</h1>
+      <img src="https://avatars.mds.yandex.net/i?id=b3e978d60b8a94b82683f084739be6e128b5584f-5887026-images-thumbs&n=13" alt="dsd" />
+    </div>
+  )
+}
+export default Base
